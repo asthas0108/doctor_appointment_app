43f904c8ba9ed40a7a2500561842af2dd567b488
asthas0108 <asthas0108@gmail.com>
2024-08-01 15:35:13 +0530

    login and register page

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import "../styles/RegisterStyle.css";
+import { Form, Input, message } from "antd";
+import {Link, useNavigate} from "react-router-dom";
+
 
 const login = () => {
-  return (
-    <div>
-        
-    </div>
-  )
-}
 
-export default login
+  //form handler
+  const onfinishHandler= async (values)=>{
+    console.log(values);
+  }
+
+  return (
+    <div className="form-container ">
+      <Form layout="vertical" onFinish={onfinishHandler} className="register-form">
+        <h3 className="text-center">LOGIN FORM</h3>
+
+        <Form.Item label="Email" name="email">
+          <Input type="email" required />
+        </Form.Item>
+
+        <Form.Item label="Password" name="password">
+          <Input type="password" required />
+        </Form.Item>
+
+        <Link to="/register" className="m-2">
+          Not a user ? Register here
+        </Link>
+
+        <button className="btn btn-primary" type="submit">
+          Login
+        </button>
+      </Form>
+    </div>
+  );
+};
+export default login;
--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,12 +1,43 @@
 import React from 'react';
-// import { Form } from "antd";
-
-
+import { Form, Input } from "antd";
+import "../styles/RegisterStyle.css";
+import {Link, useNavigate} from "react-router-dom";
 
 const register = () => {
+
+  //form handler
+  const onfinishHandler= async (values)=>{
+    console.log(values);
+  }
+
   return (
     <>
-      
+      <div className="form-container">
+        <Form layout="vertical" onFinish={onfinishHandler} className="card p-4">
+
+          <h2 className="text-center">REGISTRATION FORM</h2>
+
+          <Form.Item label="Name" name="name">
+            <Input type="text" required/>
+          </Form.Item>
+
+          <Form.Item label="Email" name="email">
+            <Input type="email" required/>
+          </Form.Item>
+
+          <Form.Item label="Password" name="password">
+            <Input type="password" required/>
+          </Form.Item>
+
+          <Link to="/login" className="m-2">
+          Already a user, then login here. 
+          </Link>
+
+          <button className="btn btn-primary" type="submit">
+            Register
+          </button>
+        </Form>
+      </div>
     </>
   )
 }
