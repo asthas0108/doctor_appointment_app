6645492772dd98661b9e5076e35bf4d411d3fd53
asthas0108 <asthas0108@gmail.com>
2024-08-01 12:11:25 +0530

    initial frontend setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import HomePage from "./pages/homepage";
+import Login from "./pages/login";
+import Register from "./pages/register";
+
 function App() {
   return (
     <>
-    
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/" element={<Login/>}/>
+          <Route path="/" element={<Register/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+// import { Form } from "antd";
+
+
 
 const register = () => {
   return (
-    <div>
-        
-    </div>
+    <>
+      
+    </>
   )
 }
 
